dd893b982e769d98ca99f8ab0261e83a26b63739
evgvalo <valov.eu@yandex.ru>
2021-05-22 11:58:19 +0300

    add stack Final stack

--- Tasks/a0_my_stack.py
+++ Tasks/a0_my_stack.py
@@ -12,8 +12,8 @@ def push(elem: Any) -> None:
     :param elem: element to be pushed
     :return: Nothing
     """
-    print(elem)
-    return None
+    _stack.append(elem)
+
 
 
 def pop() -> Any:
@@ -22,7 +22,8 @@ def pop() -> Any:
 
     :return: popped element
     """
-    return None
+    return _stack.pop(-1) if _stack else None
+
 
 
 def peek(ind: int = 0) -> Any:
@@ -32,8 +33,9 @@ def peek(ind: int = 0) -> Any:
     :param ind: index of element (count from the top, 0 - top, 1 - first from top, etc.)
     :return: peeked element or None if no element in this place
     """
-    print(ind)
-    return None
+
+    if ind < len(_stack):
+        return _stack[-ind-1]
 
 
 def clear() -> None:
@@ -42,4 +44,6 @@ def clear() -> None:
 
     :return: None
     """
-    return None
+    _stack.clear()
+
+

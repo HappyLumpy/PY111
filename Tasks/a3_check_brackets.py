8b6a2331eab225ea52b3874f218c2555da16c0a5
valov <valov.eu@yandex.ru>
2021-05-23 20:02:37 +0300

    check_brackets(через регулярку)

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -13,8 +13,8 @@ def check_brackets(brackets_row: str) -> bool:
     for brackets in brackets_row:
         if brackets == '(':
             count_open_brackets += 1
-            if brackets == ')':
-                count_close_brackets += 1
+        if brackets == ')':
+            count_close_brackets += 1
     if count_open_brackets == count_close_brackets:
         if re.fullmatch(r'\(.*\)', brackets_row) or brackets_row == "":
             return True

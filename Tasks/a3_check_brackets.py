27a5d0f84f125e295ba503e48ccb5f6a85698cf9
valov <valov.eu@yandex.ru>
2021-05-23 19:56:35 +0300

    check_brackets(через регулярку)

--- Tasks/a3_check_brackets.py
+++ Tasks/a3_check_brackets.py
@@ -1,3 +1,6 @@
+import re
+
+
 def check_brackets(brackets_row: str) -> bool:
     """
     Check whether input string is a valid bracket sequence
@@ -5,5 +8,15 @@ def check_brackets(brackets_row: str) -> bool:
     :param brackets_row: input string to be checked
     :return: True if valid, False otherwise
     """
-
-    return False
+    count_open_brackets = 0
+    count_close_brackets = 0
+    for brackets in brackets_row:
+        if brackets == '(':
+            count_open_brackets += 1
+            if brackets == ')':
+                count_close_brackets += 1
+    if count_open_brackets == count_close_brackets:
+        if re.fullmatch(r'\(.*\)', brackets_row) or brackets_row == "":
+            return True
+    else:
+        return False

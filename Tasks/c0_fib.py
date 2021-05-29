cc6c168fd3a2ed81339a1ce6c578e46f93072c28
evgvalo <valov.eu@yandex.ru>
2021-05-29 12:37:44 +0300

    add fib

--- Tasks/c0_fib.py
+++ Tasks/c0_fib.py
@@ -5,8 +5,12 @@ def fib_recursive(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n >= 0:
+        if n in (0, 1):
+            return n
+        return fib_recursive(n - 1) + fib_recursive(n - 2)
+    else:
+        raise ValueError
 
 
 def fib_iterative(n: int) -> int:
@@ -16,5 +20,21 @@ def fib_iterative(n: int) -> int:
     :param n: number of item
     :return: Fibonacci number
     """
-    print(n)
-    return 0
+    if n >= 0:
+        f1 = 1
+        f2 = 1
+        i = 0
+        f_sum = 0
+        while i < n - 2:
+            f_sum = f1 + f2
+            f1 = f2
+            f2 = f_sum
+            i = i + 1
+        return f_sum
+    else:
+        raise ValueError
+
+
+if __name__ == '__main__':
+    print(fib_iterative(-50))
+    print(fib_recursive(-5))

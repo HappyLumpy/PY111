a3ed8fedfabc0aaf3a93840d5b17982a549e23a2
valov <valov.eu@yandex.ru>
2021-06-14 22:42:52 +0300

    Бинарный поиск через рекурсию(не рабочий)

--- Tasks/c2_recursive_binary_search.py
+++ Tasks/c2_recursive_binary_search.py
@@ -9,5 +9,24 @@ def binary_search(elem: int, arr: Sequence) -> Optional[int]:
     :param arr: array where element is to be found
     :return: Index of element if it's presented in the arr, None otherwise
     """
-    print(elem, arr)
-    return None
+
+    fist = 0
+    last = len(arr) - 1
+    mid = (fist + last) // 2
+    print(arr)
+    if arr[mid] == elem:
+        return mid
+    else:
+        if elem < arr[mid] != elem:
+            last = mid - 1
+            arr = arr[:last]
+            return binary_search(elem, arr)
+        else:
+            fist = mid + 1
+            arr = arr[fist:]
+            return binary_search(elem, arr)
+
+
+if __name__ == '__main__':
+    list_ = list(range(0, 11))
+    binary_search(8, list_)

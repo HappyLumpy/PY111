9061405e2fa97313e12c153cac9c2b7cd8cc61ac
valov <valov.eu@yandex.ru>
2021-06-18 22:21:20 +0300

    Taylor(тесты проходит но с синусом есть вопросы)

--- Tasks/b2_Taylor.py
+++ Tasks/b2_Taylor.py
@@ -2,6 +2,7 @@
 Taylor series
 """
 from typing import Union
+import math
 
 
 def ex(x: Union[int, float]) -> float:
@@ -11,8 +12,10 @@ def ex(x: Union[int, float]) -> float:
     :param x: x value
     :return: e^x value
     """
-    print(x)
-    return 0
+    e = 0
+    for i in range(0, 100):
+        e = e + (x ** i) / math.factorial(i)
+    return e
 
 
 def sinx(x: Union[int, float]) -> float:
@@ -22,5 +25,13 @@ def sinx(x: Union[int, float]) -> float:
     :param x: x value
     :return: sin(x) value
     """
-    print(x)
-    return 0
+    e = 0
+    for i in range(0, 50):
+        e = e + (((-1) ** i) * (x ** (1 + 2 * i))) / math.factorial(1 + 2 * i)
+    return e
+
+
+if __name__ == '__main__':
+    print(sinx(1.55433))
+    print(math.sin(1.55433))
+    print(ex(6))

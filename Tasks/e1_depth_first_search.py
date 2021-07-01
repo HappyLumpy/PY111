193f5cf43f3ed060daff876b6af1e0a14a7755aa
valov <valov.eu@yandex.ru>
2021-07-01 19:26:32 +0300

    Обход в глубину

--- Tasks/e1_depth_first_search.py
+++ Tasks/e1_depth_first_search.py
@@ -2,7 +2,7 @@ from typing import Hashable, List
 import networkx as nx
 
 
-def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
+def dfs(g: nx.Graph, start_node: Hashable, used = None) -> List[Hashable]:
     """
     Do an depth-first search and returns list of nodes in the visited order
 
@@ -10,5 +10,29 @@ def dfs(g: nx.Graph, start_node: Hashable) -> List[Hashable]:
     :param start_node: starting node of search
     :return: list of nodes in the visited order
     """
-    print(g, start_node)
-    return list(g.nodes)
+    if used is None:
+        used = []
+    used.append(start_node)
+    for neighbor in g[start_node]:
+        if neighbor not in used:
+            dfs(g, neighbor, used)
+    return used
+
+
+if __name__ == '__main__':
+    graph = nx.Graph()
+    graph.add_nodes_from("ABCDEFG")
+    graph.add_edges_from([
+        ('A', 'B'),
+        ('A', 'C'),
+        ('B', 'D'),
+        ('B', 'E'),
+        ('C', 'F'),
+        ('E', 'G'),
+    ])
+    print(dfs(graph,'A'))
+
+    list('ABDEGCF'),
+    list('ABEGDCF'),
+    list('ACFBDEG'),
+    list('ACFBEGD')

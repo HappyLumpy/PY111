4e8cf1130807787850e0fc4b4cc1f5e01d8c8eb0
valov <valov.eu@yandex.ru>
2021-07-02 22:17:19 +0300

    dijkstra без значения float('inf')

--- Tasks/e2_dijkstra.py
+++ Tasks/e2_dijkstra.py
@@ -1,5 +1,7 @@
 from typing import Hashable, Mapping, Union
 import networkx as nx
+from collections import deque
+import matplotlib
 
 
 def dijkstra_algo(g: nx.DiGraph, starting_node: Hashable) -> Mapping[Hashable, Union[int, float]]:
@@ -9,5 +11,33 @@ def dijkstra_algo(g: nx.DiGraph, starting_node: Hashable) -> Mapping[Hashable, U
     :param starting_node: starting node from g
     :return: dict like {'node1': 0, 'node2': 10, '3': 33, ...} with path costs, where nodes are nodes from g
     """
-    print(g, starting_node)
-    return dict()
+    queue = deque()
+    s = {starting_node: 0}
+    queue.append(starting_node)
+    while queue:
+        v = queue.pop()
+        for u in g[v]:
+            if u not in s or s[v] + g[v][u]['weight'] < s[u]:
+                s[u] = s[v] + g[v][u]['weight']
+                queue.append(u)
+    return s
+
+
+if __name__ == '__main__':
+    G = nx.DiGraph()
+    G.add_nodes_from("ABCDEFG")
+    G.add_weighted_edges_from([
+        ("A", "B", 1),
+        ("B", "C", 3),
+        ("C", "E", 4),
+        ("E", "F", 3),
+        ("B", "E", 8),
+        ("C", "D", 1),
+        ("D", "E", 2),
+        ("B", "D", 2),
+        ("G", "D", 1),
+        ("D", "A", 2),
+    ])
+    print(dijkstra_algo(G, "A"))
+    nx.draw(G)
+# {'A': 3, 'B': 4, 'C': 7, 'D': 1, 'E': 3, 'F': 6, 'G': 0}

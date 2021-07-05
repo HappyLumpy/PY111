df628bf45fd39cda3427f7b1615070ed97156f96
valov <valov.eu@yandex.ru>
2021-07-05 21:16:02 +0300

    Лошадь рабочая

--- Tasks/d1_horse.py
+++ Tasks/d1_horse.py
@@ -15,20 +15,21 @@ def calculate_paths(shape: (int, int), point: (int, int)) -> int:
     for i in range(shape[0]):
         field.append([0] * shape[1])
     field[0][0] = 1
+    sum_ = 0
     for y in range(len(field)):
         for x in range(len(field[y])):
             if field[y][x] != 0:
-                if 0 < y+1 < len(field) and 0 < x+2 < len(field[y]):
-                    field[y+1][x+2] += 2
-                if 0 < y + 2 < len(field)  and 0 < x + 1 < len(field[y]):
-                    field[y + 2][x + 1] += 2
-                if 0 < y + 1 < len(field) and 0 < x - 2 < len(field[y]):
-                    field[y + 1][x -2] += 2
-                if 0 < y + 2 < len(field) and 0 < x - 1 < len(field[y]):
-                    field[y + 2][x -1] += 2
-    return field
+                if 0 <= y+1 < len(field) and 0 <= x+2 < len(field[y]):
+                    field[y+1][x+2] = 2 * field[y][x] + field[y+1][x+2]
+                if 0 <= y + 2 < len(field)  and 0 <= x + 1 < len(field[y]):
+                    field[y + 2][x + 1] = 2  * field[y][x] + field[y + 2][x + 1]
+                if 0 <= y + 1 < len(field) and 0 <= x - 2 < len(field[y]):
+                    field[y + 1][x -2] = 2  * field[y][x] + field[y + 1][x -2]
+                if 0 <= y + 2 < len(field) and 0 <= x - 1 < len(field[y]):
+                    field[y + 2][x -1] = 2  * field[y][x] + field[y + 2][x -1]
+    return field[point[0]][point[1]]
 
 
 if __name__ == '__main__':
-    a = calculate_paths((8,8),(8,8))
-    print_field(a)
+    a = calculate_paths((8,8),(7,7))
+    print(a)
